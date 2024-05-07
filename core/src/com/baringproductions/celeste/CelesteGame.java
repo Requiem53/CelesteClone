9682cf46d0e4c30ad9520ef30dc886c0985d4b02
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-05-07 21:24:01 +0800

    nay platform ug guy

--- core/src/com/baringproductions/celeste/CelesteGame.java
+++ core/src/com/baringproductions/celeste/CelesteGame.java
@@ -38,6 +38,62 @@ public class CelesteGame extends ApplicationAdapter {
 		imageRep.y = 50;
 		imageRep.width = 16;
 		imageRep.height = 16;
+
+		// First we create a body definition
+		BodyDef bodyDef = new BodyDef();
+// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+// Set our body's starting position in the world
+		bodyDef.position.set((float) Gdx.graphics.getDisplayMode().width / 2 - (float) 64 /2, 500);
+
+// Create our body in the world using our body definition
+		Body body = world.createBody(bodyDef);
+
+// Create a circle shape and set its radius to 6
+		CircleShape circle = new CircleShape();
+		circle.setRadius(16f);
+
+// Create a fixture definition to apply our shape to
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f;
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+// Create our fixture and attach it to the body
+		Fixture fixture = body.createFixture(fixtureDef);
+
+// Remember to dispose of any shapes after you're done with them!
+// BodyDef and FixtureDef don't need disposing, but shapes do.
+		circle.dispose();
+
+		// First we create a body definition
+		BodyDef platform = new BodyDef();
+// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+		platform.type = BodyDef.BodyType.StaticBody;
+// Set our body's starting position in the world
+		platform.position.set((float) Gdx.graphics.getDisplayMode().width / 2 - (float) 64 /2, 200);
+
+// Create our body in the world using our body definition
+		Body body2 = world.createBody(platform);
+
+// Create a circle shape and set its radius to 6
+		PolygonShape rect = new PolygonShape();
+		rect.setAsBox(100f, 50f);
+
+// Create a fixture definition to apply our shape to
+		FixtureDef fixtureDef2 = new FixtureDef();
+		fixtureDef2.shape = rect;
+		fixtureDef2.density = 0.5f;
+		fixtureDef2.friction = 0.4f;
+		fixtureDef2.restitution = 0.6f; // Make it bounce a little bit
+
+// Create our fixture and attach it to the body
+		Fixture fixture2 = body2.createFixture(fixtureDef2);
+
+// Remember to dispose of any shapes after you're done with them!
+// BodyDef and FixtureDef don't need disposing, but shapes do.
+		rect.dispose();
 	}
 
 	@Override
