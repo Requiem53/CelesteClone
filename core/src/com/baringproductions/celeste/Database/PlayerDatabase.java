9e2f7777682c25d2420f4ce12c14c67079d79bc1
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-05-28 11:09:53 +0800

    sql?

--- core/src/com/baringproductions/celeste/Database/PlayerDatabase.java
+++ core/src/com/baringproductions/celeste/Database/PlayerDatabase.java
@@ -2,25 +2,30 @@ package com.baringproductions.celeste.Database;
 
 import com.baringproductions.celeste.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PlayerDatabase {
     private static String name;
     private static User user;
     private static int spawn;
-    public static void loadPlayer() {
+    public static void initTables() {
         try (Connection c = MySQLConnection.getConnection();
-             PreparedStatement statement = c.prepareStatement(
-                     "CREATE TABLE IF NOT EXISTS tbluser (" +
-                             "id INT AUTO_INCREMENT PRIMARY KEY," +
-                             "name VARCHAR(50) NOT NULL," +
-                             "email VARCHAR(100) NOT NULL," +
-                             "password VARCHAR(50) )");) {
-            statement.addBatch();
-            // error
+             Statement statement = c.createStatement()) {
+
+            c.setAutoCommit(false);
+
+            statement.addBatch("CREATE TABLE IF NOT EXISTS tbluser (" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "name VARCHAR(20) NOT NULL," +
+                    "spawn INT(100) NOT NULL)");
+            statement.addBatch("CREATE TABLE IF NOT EXISTS tbluserberry (" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "userid INT NOT NULL," +
+                    "x INT(10) NOT NULL," +
+                    "y INT(10) NOT NULL," +
+                    "FOREIGN KEY (userid) REFERENCES tbluser(id) ON UPDATE CASCADE ON DELETE CASCADE)");
+
+            statement.executeBatch();
             System.out.println("TABLE created successfully");
         }catch (SQLException e) {
             e.printStackTrace();
@@ -30,28 +35,28 @@ public class PlayerDatabase {
 
     }
     public static User getNewUser(String name) {
-//        try (Connection c = MySQLConnection.getConnection();
-//             PreparedStatement statement = c.prepareStatement(
-//                     "INSERT INTO users (name) VALUES (?)");) {
-//            statement.setString(1, name);
-//            int CURRENT_USER_ID = 0;
-//            int row = statement.executeUpdate();
-//            if (row > 0) {
-//                try (ResultSet generatedKey = statement.getGeneratedKeys()) {
-//                    if (generatedKey.next()) {
-//                        CURRENT_USER_ID = generatedKey.getInt(1);
-//
-//
-//                        System.out.println("CURR ID: " + CURRENT_USER_ID);
-//                        user = new User(CURRENT_USER_ID, name);
-//                        return user;
-//                    }
-//                    return null;
-//                }
-//            }
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        try (Connection c = MySQLConnection.getConnection();
+             PreparedStatement statement = c.prepareStatement(
+                     "INSERT INTO users (name) VALUES (?)");) {
+            statement.setString(1, name);
+            int CURRENT_USER_ID = 0;
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                try (ResultSet generatedKey = statement.getGeneratedKeys()) {
+                    if (generatedKey.next()) {
+                        CURRENT_USER_ID = generatedKey.getInt(1);
+
+
+                        System.out.println("CURR ID: " + CURRENT_USER_ID);
+                        user = new User(CURRENT_USER_ID, name);
+                        return user;
+                    }
+                    return null;
+                }
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
